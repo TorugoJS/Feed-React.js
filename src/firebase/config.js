78f7774cc8174011eec219a3c981eaaede343bb5
TorugoJS <victor.devfrontend@gmail.com>
2022-09-06 20:38:12 -0300

    Feed-React

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-f6y7_h3ur-DoSMB4Pwkl2YE8cN1Q9iA",
+  authDomain: "feed-react-6e520.firebaseapp.com",
+  projectId: "feed-react-6e520",
+  storageBucket: "feed-react-6e520.appspot.com",
+  messagingSenderId: "263945363115",
+  appId: "1:263945363115:web:68e9c6a40e46a36293a1ee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
