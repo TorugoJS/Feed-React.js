640a00b5e9b1d9113c0ec1649b3a9b4a930e922f
TorugoJS <victor.devfrontend@gmail.com>
2022-09-08 20:47:54 -0300

    Network-of-React

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -6,8 +6,49 @@ import { useState, useEffect } from 'react';
 
 const Register = () => {
 
-  return (
 
+  // useState
+  const [displayName, setDisplayName] = useState("")  // para manipular o name 
+  const [email, setEmail] = useState("") // para manipular o email
+  const [password, setPassword] = useState("") // para manipular a senha
+  const [confirmPassword, setConfirmPassword] = useState("") // para confirmar a senha
+  const [error, setError] = useState("") // para manipular o erros
+
+  // reunir todos os dados e enviar no formulário.
+  const handleSubmit = (e) => {
+    e.preventDefault(); // para enviar o formulário sem fazer a recarregamento da página.
+
+
+    setError(""); // zerando os erros ao envio do formulário - não exibirá nenhum erro
+
+
+    // formando um usuário baseado nos inputs
+    // criando um objeto com informações do usuário
+    const user = {
+      displayName,
+      email,
+      password,
+    };
+
+
+    // 1º VALIDAÇÃO - SENHA
+    // Validação - se a senha for diferente da confirmação da senha =>
+    // Exiba erro ("As senhas precisam ser iguais")
+    if (password !== confirmPassword) {
+      setError("As senhas precisam ser iguais")
+      return;
+    }
+
+    //imprimindo objeto com informações do usuário
+    console.log(user);
+  };
+
+
+
+
+
+  return (
+      //container com form
     <div className={styles.register}>
 
       {/* Título da pagina de registrar */}
@@ -15,14 +56,19 @@ const Register = () => {
       <p>Compartilhe suas histórias e ganhe seguidores!</p>
 
       {/* Formulário */}
-      <form>
+      {/* Chamando função de envio de formulário */}
+      <form onSubmit={handleSubmit}>
 
         {/* Form usário */}
         <label>
           <span>Nome:</span>
           <input type="text"
             name="displayName"
-            required placeholder="Nome do usuário" />
+            required
+            placeholder="Nome do usuário"
+            value={displayName} // linkando ao useState e controlando valor
+            onChange={(e) => setDisplayName(e.target.value)} // preenchendo valores dos states com onChange
+          />
         </label>
 
         {/* Form e-mail */}
@@ -30,7 +76,11 @@ const Register = () => {
           <span>E-mail:</span>
           <input type="email"
             name="email"
-            required placeholder="E-mail do usuário" />
+            required
+            placeholder="E-mail do usuário"
+            value={email} // linkando ao useState e controlando valor
+            onChange={(e) => setEmail(e.target.value)} // preenchendo valores dos states com onChange
+          />
         </label>
 
         {/* Form senha */}
@@ -38,7 +88,11 @@ const Register = () => {
           <span>Senha:</span>
           <input type="password"
             name="password"
-            required placeholder="Senha do usuário" />
+            required
+            placeholder="Senha do usuário"
+            value={password} // linkando ao useState e controlando valor
+            onChange={(e) => setPassword(e.target.value)} // preenchendo valores dos states com onChange
+          />
         </label>
 
         {/* Form confirmar a senha */}
@@ -46,11 +100,20 @@ const Register = () => {
           <span>Confirmar senha:</span>
           <input type="password"
             name="confirmPassword"
-            required placeholder="Confirme a sua senha" />
+            required placeholder="Confirme a sua senha"
+            value={confirmPassword} // linkando ao useState e controlando valor
+            onChange={(e) => setConfirmPassword(e.target.value)} // preenchendo valores dos states com onChange
+          />
         </label>
 
         {/* Botão para enviar formulário. */}
         <button className='btn'>Cadastrar</button>
+
+
+        {/* se houve alguem erro no formulário exibirá o erro */}
+        {/* condição que será executada sendo true */}
+        {error && <p className="error">{error}</p>}
+
 
       </form>
 
