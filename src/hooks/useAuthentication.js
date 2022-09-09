74c138fdf52a41bd277290a21ffeff32acca3657
TorugoJS <victor.devfrontend@gmail.com>
2022-09-08 21:07:31 -0300

    Network-of-React

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -9,20 +9,31 @@ import {
 
 
 //
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
-//função do hook
-//exportando
-//autenticação
+// função do hook
+// exportando
+// autenticação
 export const useAuthentication = () => {
-    //1º estágio
+
+    // 1º estágio de states
     const [error, setError] = useState(null) // erros
     const [loading, setLoading] = useState(null) // carregamento
 
     // cleanUp => não vai deixar resquicios de funções para não ter problema de memória
     // deal with memory leak
-
+    // state para cancelamento
     const [cancelled, setCancelled] = useState(false) // iniciando como false
 
+    // pegando autenticação do firebase
+    const auth = getAuth()
 
-}
+    // criando um função para checar o state e reutilizar ela depois
+    // se for true retorne
+    // será o clearUp do vazamento de memória
+    function checkIfIsCancelled() {
+        if (cancelled) {
+            return;
+        }
+    }
+};
