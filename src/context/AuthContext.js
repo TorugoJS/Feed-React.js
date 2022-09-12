f5498a28eac9db6a3595e13f9cc4d924fb4ff614
TorugoJS <victor.devfrontend@gmail.com>
2022-09-12 20:33:46 -0300

    Network-of-React

---
+++ src/context/AuthContext.js
@@ -0,0 +1,7 @@
+import { useContext, createContext } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider ({children, value}) {
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
