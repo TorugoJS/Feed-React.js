ef1df1f26b8bd08e2c58a680f75c3ad1f44cfa6a
TorugoJS <victor.devfrontend@gmail.com>
2022-09-20 20:13:46 -0300

    Network-of-React

---
+++ src/hooks/useFetchDocuments.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import { db } from "../firebase/config";
+import {
+    collection, // coleção
+    query, // pegar o dado
+    orderBy, // ordenação
+    onSnapshot,
+    where // filtro dos detalhes que estão vindo
+} from 'firebase/firestore';
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+
+    const [documents, setDocuments] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+
+    // deal with memory leak
+    const [cancelled, setCancelled] = useState(false)
+
+    useEffect(() => {
+
+        async function loadData() {
+            if (cancelled) return;
+
+            setLoading(true)
+
+            const collectionRef = await collection(db, docCollection)
+
+            try {
+                // criando a busca de dados
+                let q;
+
+                //busca
+                //dashboard
+
+                q = await query(collectionRef, orderBy("createdAt", "desc"));
+
+                await onSnapshot(q, (querySnapshot) => {
+                    setDocuments(
+                        querySnapshot.docs.map((doc) => ({
+                            id: doc.id,
+                            ...doc.data(),
+                        }))
+                    )
+                });
+
+                setLoading(false);
+
+            } catch (error) {
+                console.log(error)
+                setError(error.message)
+
+                setLoading(false);
+            }
+
+        }
+
+        loadData();
+    }, [docCollection, search, uid, cancelled]);
+
+    useEffect(() => {
+        return () => setCancelled(true)
+    }, []);
+
+    return documents, loading, error;
+
+};
