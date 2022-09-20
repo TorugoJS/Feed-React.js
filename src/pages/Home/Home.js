170e15e82062b857b77dd12e7c7e257bf1157a0e
TorugoJS <victor.devfrontend@gmail.com>
2022-09-20 19:53:24 -0300

    Network-of-React

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,47 @@
 // CSS
 import styles from "./Home.module.css";
 
+// hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
 const Home = () => {
+
+  //fazendo a busca da barra de pesquisa
+  const [query, setQuery] = useState("");
+
+  //sem posts
+  const [posts] = useState([]);
+
+  // função de envio
+  // parando o carregamento da página com preventDefault
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+
   return (
-    <div>
-        <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os posts recentes!</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input type="text"
+          placeholder="Busque por tags"  // alterando state quando for digitado algo.
+          onChange={(e) => setQuery(e.target.value)} />
+        <button className="btn">Pequisar</button>
+      </form>
+
+
+      {/* Lista de post */}
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não há post</p>
+            <Link to="/post/create" className="btn">Criar post</Link>
+          </div>
+        )}
+      </div>
+
     </div>
   )
 }
